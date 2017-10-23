1e5b4fa3f146949ed7cc0417aca8a049feb13f2c
MariiaGalay <olenagalay.lviv@gmail.com>
2017-10-23 21:37:11 +0300

    upload project

---
+++ src/test/java/TestAppium.java
@@ -0,0 +1,66 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class TestAppium {
+    WebDriver driver;
+    @Before
+    public void start() throws MalformedURLException {
+        // Created object of DesiredCapabilities class.
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        // Set android deviceName desired capability. Set your device name.
+        capabilities.setCapability("deviceName", "M3s");
+
+        // Set BROWSER_NAME desired capability. It's Android in our case here.
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
+
+        // Set android VERSION desired capability. Set your mobile device's OS version.
+        capabilities.setCapability(CapabilityType.VERSION, "5.0");
+
+        // Set android platformName desired capability. It's Android in our case here.
+        capabilities.setCapability("platformName", "Android");
+
+        // Set android appPackage desired capability. It is
+        // com.android.calculator2 for calculator application.
+        // Set your application's appPackage if you are using any other app.
+        capabilities.setCapability("appPackage", "com.android.email");
+
+        // Set android appActivity desired capability. It is
+        // com.android.calculator2.Calculator for calculator application.
+        // Set your application's appPackage if you are using any other app.
+        capabilities.setCapability("appActivity", ".activity.EmailActivity");
+
+        // Created object of RemoteWebDriver will all set capabilities.
+        // Set appium server address and port number in URL string.
+        // It will launch calculator app in android device.
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testMail() {
+
+        driver.findElement(By.xpath("//android.widget.LinearLayout[@content-desc=\"Google GMail\"]")).click();
+        driver.findElement(By.
+                xpath("//android.widget.EditText[@content-desc=\"Email or phone\"]"))
+                .sendKeys("2963424@gmail.com");
+
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Next\"]")).click();
+        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+    }
+
+    @After
+    public void finish() {
+        driver.quit();
+    }
+}
